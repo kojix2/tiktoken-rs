b308991427f30d4a78739ed5f31b649eb30f933c
Roger Zurawicki <zurawiki@users.noreply.github.com>
2025-11-09 15:23:58 -0500

    Add: o200k_harmony tokenizer for gpt-oss. (#123)
    Co-authored-by: CaffeeLake <PascalCoffeeLake@gmail.com>

--- tiktoken-rs/src/singleton.rs
+++ tiktoken-rs/src/singleton.rs
@@ -2,7 +2,7 @@ use lazy_static::lazy_static;
 
 use crate::vendor_tiktoken::CoreBPE;
 
-use crate::{cl100k_base, o200k_base, p50k_base, p50k_edit, r50k_base};
+use crate::{cl100k_base, o200k_base, o200k_harmony, p50k_base, p50k_edit, r50k_base};
 
 /// Returns a singleton instance of the r50k_base tokenizer. (also known as `gpt2`)
 /// Use for GPT-3 models like `davinci`
@@ -49,7 +49,7 @@ pub fn cl100k_base_singleton() -> &'static CoreBPE {
 }
 
 /// Returns a singleton instance of the o200k_base tokenizer.
-/// Use for GPT-4o models and other `o` series models like `o1`, `o3`, and `o4`.
+/// Use for GPT-5, GPT-4.1, GPT-4o, and other `o` series models like `o1`, `o3`, and `o4`.
 ///
 /// This function will only initialize the tokenizer once, and then return a reference the tokenizer
 pub fn o200k_base_singleton() -> &'static CoreBPE {
@@ -57,4 +57,15 @@ pub fn o200k_base_singleton() -> &'static CoreBPE {
         static ref O200K_BASE: CoreBPE = o200k_base().unwrap();
     }
     &O200K_BASE
+}
+
+/// Returns a singleton instance of the o200k_harmony tokenizer.
+/// Use for gpt-oss models like `gpt-oss-20b`, `gpt-oss-120b`.
+///
+/// This function will only initialize the tokenizer once, and then return a reference the tokenizer
+pub fn o200k_harmony_singleton() -> &'static CoreBPE {
+    lazy_static! {
+        static ref O200K_HARMONY: CoreBPE = o200k_harmony().unwrap();
+    }
+    &O200K_HARMONY
 }
--- tiktoken-rs/src/tiktoken_ext/openai_public.rs
+++ tiktoken-rs/src/tiktoken_ext/openai_public.rs
@@ -118,16 +118,28 @@ pub fn cl100k_base() -> Result<CoreBPE> {
     Ok(bpe)
 }
 
-/// Use for GPT-4o models and other `o` series models like `o1`, `o3`, and `o4`.
+pub const O200K_BASE_PAT_STR: &str = concat!(
+    r#"[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]*[\p{Ll}\p{Lm}\p{Lo}\p{M}]+(?i:'s|'t|'re|'ve|'m|'ll|'d)?"#,
+    "|",
+    r#"[^\r\n\p{L}\p{N}]?[\p{Lu}\p{Lt}\p{Lm}\p{Lo}\p{M}]+[\p{Ll}\p{Lm}\p{Lo}\p{M}]*(?i:'s|'t|'re|'ve|'m|'ll|'d)?"#,
+    "|",
+    r#"\p{N}{1,3}"#,
+    "|",
+    r#" ?[^\s\p{L}\p{N}]+[\r\n/]*"#,
+    "|",
+    r#"\s*[\r\n]+"#,
+    "|",
+    r#"\s+(?!\S)"#,
+    "|",
+    r#"\s+"#
+);
+
+/// Use for GPT-5, GPT-4.1, GPT-4o, and other `o` series models like `o1`, `o3`, and `o4`.
 /// Initializes and returns a new instance of the o200k_base tokenizer.
 pub fn o200k_base() -> Result<CoreBPE> {
     let o200k_base = include_str!("../../assets/o200k_base.tiktoken");
 
-    let mut encoder: std::collections::HashMap<
-        Vec<u8>,
-        Rank,
-        std::hash::BuildHasherDefault<rustc_hash::FxHasher>,
-    > = HashMap::default();
+    let mut encoder = HashMap::default();
     for line in o200k_base.lines() {
         let mut parts = line.split(' ');
         let raw = parts.next().unwrap();
@@ -140,18 +152,46 @@ pub fn o200k_base() -> Result<CoreBPE> {
     special_tokens.insert(String::from(ENDOFTEXT), 199999);
     special_tokens.insert(String::from(ENDOFPROMPT), 200018);
 
-    let bpe = CoreBPE::new(
-        encoder,
-        special_tokens,
-        &[
-            "[^\\r\\n\\p{L}\\p{N}]?[\\p{Lu}\\p{Lt}\\p{Lm}\\p{Lo}\\p{M}]*[\\p{Ll}\\p{Lm}\\p{Lo}\\p{M}]+(?i:'s|'t|'re|'ve|'m|'ll|'d)?",
-            "[^\\r\\n\\p{L}\\p{N}]?[\\p{Lu}\\p{Lt}\\p{Lm}\\p{Lo}\\p{M}]+[\\p{Ll}\\p{Lm}\\p{Lo}\\p{M}]*(?i:'s|'t|'re|'ve|'m|'ll|'d)?",
-            "\\p{N}{1,3}",
-            " ?[^\\s\\p{L}\\p{N}]+[\\r\\n/]*",
-            "\\s*[\\r\\n]+",
-            "\\s+(?!\\S)",
-            "\\s+",
-        ].join("|"),
-    )?;
+    let bpe = CoreBPE::new(encoder, special_tokens, O200K_BASE_PAT_STR)?;
+    Ok(bpe)
+}
+
+/// Use for gpt-oss models like `gpt-oss-20b`, `gpt-oss-120b`.
+/// Initializes and returns a new instance of the o200k_harmony tokenizer.
+pub fn o200k_harmony() -> Result<CoreBPE> {
+    let o200k_harmony = include_str!("../../assets/o200k_base.tiktoken");
+
+    let mut encoder = HashMap::default();
+    for line in o200k_harmony.lines() {
+        let mut parts = line.split(' ');
+        let raw = parts.next().unwrap();
+        let token = &general_purpose::STANDARD.decode(raw)?;
+        let rank: Rank = parts.next().unwrap().parse().unwrap();
+        encoder.insert(token.clone(), rank);
+    }
+
+    let mut special_tokens = HashMap::default();
+
+    special_tokens.insert(String::from("<|startoftext|>"), 199998);
+    special_tokens.insert(String::from("<|endoftext|>"), 199999);
+    special_tokens.insert(String::from("<|reserved_200000|>"), 200000);
+    special_tokens.insert(String::from("<|reserved_200001|>"), 200001);
+    special_tokens.insert(String::from("<|return|>"), 200002);
+    special_tokens.insert(String::from("<|constrain|>"), 200003);
+    special_tokens.insert(String::from("<|reserved_200004|>"), 200004);
+    special_tokens.insert(String::from("<|channel|>"), 200005);
+    special_tokens.insert(String::from("<|start|>"), 200006);
+    special_tokens.insert(String::from("<|end|>"), 200007);
+    special_tokens.insert(String::from("<|message|>"), 200008);
+    special_tokens.insert(String::from("<|reserved_200009|>"), 200009);
+    special_tokens.insert(String::from("<|reserved_200010|>"), 200010);
+    special_tokens.insert(String::from("<|reserved_200011|>"), 200011);
+    special_tokens.insert(String::from("<|call|>"), 200012);
+    for i in 200013..=201087 {
+        // reserved tokens from 200013 to 201087
+        special_tokens.insert(format!("<|reserved_{}|>", i), i);
+    }
+
+    let bpe = CoreBPE::new(encoder, special_tokens, O200K_BASE_PAT_STR)?;
     Ok(bpe)
 }
